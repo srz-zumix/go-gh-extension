5bd0582f367fce4a0dbedf388169eef8b8b76e5f
srz_zumix <zumix.cpp@gmail.com>
2025-06-29 01:49:32 +0900

    update pr and label api

--- pkg/gh/client/issues.go
+++ pkg/gh/client/issues.go
@@ -14,3 +14,36 @@ func (g *GitHubClient) GetIssueByNumber(ctx context.Context, owner, repo string,
 	}
 	return issue, nil
 }
+
+// SetPullRequestLabels sets labels for a pull request
+func (g *GitHubClient) ReplaceIssueLabels(ctx context.Context, owner string, repo string, number int, labels []string) ([]*github.Label, error) {
+	result, _, err := g.client.Issues.ReplaceLabelsForIssue(ctx, owner, repo, number, labels)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (g *GitHubClient) AddIssueLabels(ctx context.Context, owner string, repo string, number int, labels []string) ([]*github.Label, error) {
+	result, _, err := g.client.Issues.AddLabelsToIssue(ctx, owner, repo, number, labels)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (g *GitHubClient) RemoveIssueLabel(ctx context.Context, owner string, repo string, number int, label string) error {
+	_, err := g.client.Issues.RemoveLabelForIssue(ctx, owner, repo, number, label)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GitHubClient) ClearIssueLabels(ctx context.Context, owner string, repo string, number int) error {
+	_, err := g.client.Issues.RemoveLabelsForIssue(ctx, owner, repo, number)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/gh/pr.go
+++ pkg/gh/pr.go
@@ -8,26 +8,6 @@ import (
 	"github.com/google/go-github/v71/github"
 )
 
-func GetPullRequest(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request string) (*github.PullRequest, error) {
-	number, err := GetPullRequestNumberFromString(pull_request)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
-	}
-	pr, err := g.GetPullRequest(ctx, repo.Owner, repo.Name, number)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
-	}
-	return pr, nil
-}
-
-func GetPullRequestByNumber(ctx context.Context, g *GitHubClient, repo repository.Repository, number int) (*github.PullRequest, error) {
-	pr, err := g.GetPullRequest(ctx, repo.Owner, repo.Name, number)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
-	}
-	return pr, nil
-}
-
 func GetPullRequestNumberFromString(pr string) (int, error) {
 	number, err := GetNumberFromString(pr)
 	if err != nil {
@@ -36,8 +16,33 @@ func GetPullRequestNumberFromString(pr string) (int, error) {
 	return number, nil
 }
 
-func ListPullRequestFiles(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request string) ([]*github.CommitFile, error) {
-	number, err := GetPullRequestNumberFromString(pull_request)
+func GetPullRequestNumber(pull_request any) (int, error) {
+	switch pr := pull_request.(type) {
+	case string:
+		return GetPullRequestNumberFromString(pr)
+	case int:
+		return pr, nil
+	case *github.PullRequest:
+		return pr.GetNumber(), nil
+	default:
+		return 0, fmt.Errorf("unsupported pull request type: %T", pull_request)
+	}
+}
+
+func GetPullRequest(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any) (*github.PullRequest, error) {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	pr, err := g.GetPullRequest(ctx, repo.Owner, repo.Name, number)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+	}
+	return pr, nil
+}
+
+func ListPullRequestFiles(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any) ([]*github.CommitFile, error) {
+	number, err := GetPullRequestNumber(pull_request)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
 	}
@@ -48,30 +53,50 @@ func ListPullRequestFiles(ctx context.Context, g *GitHubClient, repo repository.
 	return files, nil
 }
 
-func ListPullRequestFilesByNumber(ctx context.Context, g *GitHubClient, repo repository.Repository, number int) ([]*github.CommitFile, error) {
-	files, err := g.ListPullRequestFiles(ctx, repo.Owner, repo.Name, number)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list files for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
-	}
-	return files, nil
-}
-
-func SetPullRequestLabels(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request string, labels []string) ([]*github.Label, error) {
-	number, err := GetPullRequestNumberFromString(pull_request)
+func SetPullRequestLabels(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, labels []string) ([]*github.Label, error) {
+	number, err := GetPullRequestNumber(pull_request)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
 	}
-	result, err := g.SetPullRequestLabels(ctx, repo.Owner, repo.Name, number, labels)
+	result, err := g.ReplaceIssueLabels(ctx, repo.Owner, repo.Name, number, labels)
 	if err != nil {
 		return nil, fmt.Errorf("failed to set labels for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
 	}
 	return result, nil
 }
 
-func SetPullRequestLabelsByNumber(ctx context.Context, g *GitHubClient, repo repository.Repository, number int, labels []string) ([]*github.Label, error) {
-	result, err := g.SetPullRequestLabels(ctx, repo.Owner, repo.Name, number, labels)
+func AddPullRequestLabels(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, labels []string) ([]*github.Label, error) {
+	number, err := GetPullRequestNumber(pull_request)
 	if err != nil {
-		return nil, fmt.Errorf("failed to set labels for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	result, err := g.AddIssueLabels(ctx, repo.Owner, repo.Name, number, labels)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add labels to pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
 	}
 	return result, nil
+}
+
+func RemovePullRequestLabel(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, label string) error {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	err = g.RemoveIssueLabel(ctx, repo.Owner, repo.Name, number, label)
+	if err != nil {
+		return fmt.Errorf("failed to remove label '%s' from pull request #%d in repository '%s/%s': %w", label, number, repo.Owner, repo.Name, err)
+	}
+	return nil
+}
+
+func ClearPullRequestLabels(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any) error {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	err = g.ClearIssueLabels(ctx, repo.Owner, repo.Name, number)
+	if err != nil {
+		return fmt.Errorf("failed to clear labels for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+	}
+	return nil
 }
