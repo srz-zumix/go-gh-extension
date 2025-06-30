745503f98f00562f57cd6b3943f18e5fae85a2d0
srz_zumix <zumix.cpp@gmail.com>
2025-06-30 12:51:59 +0900

    add milestone (#12)

---
+++ pkg/gh/client/milestone.go
@@ -0,0 +1,92 @@
+package client
+
+import (
+	"context"
+
+	"github.com/google/go-github/v71/github"
+)
+
+func (g *GitHubClient) GetMilestone(ctx context.Context, owner, repo string, number int) (*github.Milestone, error) {
+	milestone, _, err := g.client.Issues.GetMilestone(ctx, owner, repo, number)
+	if err != nil {
+		return nil, err
+	}
+	return milestone, nil
+}
+
+func (g *GitHubClient) CreateMilestone(ctx context.Context, owner, repo string, milestone *github.Milestone) (*github.Milestone, error) {
+	milestone, _, err := g.client.Issues.CreateMilestone(ctx, owner, repo, milestone)
+	if err != nil {
+		return nil, err
+	}
+	return milestone, nil
+}
+
+func (g *GitHubClient) ListMilestones(ctx context.Context, owner, repo, state, sort, direction string) ([]*github.Milestone, error) {
+	var allMilestones []*github.Milestone
+	if state == "" {
+		state = "open"
+	}
+	if sort == "" {
+		sort = "due_on"
+	}
+	if direction == "" {
+		direction = "asc"
+	}
+	opt := &github.MilestoneListOptions{
+		State:     state,
+		Sort:      sort,
+		Direction: direction,
+		ListOptions: github.ListOptions{
+			PerPage: 50,
+		},
+	}
+
+	for {
+		milestones, resp, err := g.client.Issues.ListMilestones(ctx, owner, repo, opt)
+		if err != nil {
+			return nil, err
+		}
+		allMilestones = append(allMilestones, milestones...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	return allMilestones, nil
+}
+func (g *GitHubClient) EditMilestone(ctx context.Context, owner, repo string, number int, milestone *github.Milestone) (*github.Milestone, error) {
+	milestone, _, err := g.client.Issues.EditMilestone(ctx, owner, repo, number, milestone)
+	if err != nil {
+		return nil, err
+	}
+	return milestone, nil
+}
+
+func (g *GitHubClient) DeleteMilestone(ctx context.Context, owner, repo string, number int) error {
+	_, err := g.client.Issues.DeleteMilestone(ctx, owner, repo, number)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GitHubClient) ListLabelsForMilestone(ctx context.Context, owner, repo string, number int) ([]*github.Label, error) {
+	var allLabels []*github.Label
+	opt := &github.ListOptions{PerPage: 50}
+
+	for {
+		labels, resp, err := g.client.Issues.ListLabelsForMilestone(ctx, owner, repo, number, opt)
+		if err != nil {
+			return nil, err
+		}
+		allLabels = append(allLabels, labels...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	return allLabels, nil
+}
---
+++ pkg/gh/milestone.go
@@ -0,0 +1,38 @@
+package gh
+
+import (
+	"context"
+
+	"github.com/cli/go-gh/v2/pkg/repository"
+	"github.com/google/go-github/v71/github"
+	"github.com/srz-zumix/go-gh-extension/pkg/gh/client"
+)
+
+// ListMilestones lists all milestones for a repository (wrapper)
+func ListMilestones(ctx context.Context, g *client.GitHubClient, repo repository.Repository, state, sort, direction string) ([]*github.Milestone, error) {
+	return g.ListMilestones(ctx, repo.Owner, repo.Name, state, sort, direction)
+}
+
+// GetMilestone gets a single milestone by number for a repository (wrapper)
+func GetMilestone(ctx context.Context, g *client.GitHubClient, repo repository.Repository, number int) (*github.Milestone, error) {
+	return g.GetMilestone(ctx, repo.Owner, repo.Name, number)
+}
+
+// CreateMilestone creates a new milestone for a repository (wrapper)
+func CreateMilestone(ctx context.Context, g *client.GitHubClient, repo repository.Repository, milestone *github.Milestone) (*github.Milestone, error) {
+	return g.CreateMilestone(ctx, repo.Owner, repo.Name, milestone)
+}
+
+// EditMilestone edits an existing milestone for a repository (wrapper)
+func EditMilestone(ctx context.Context, g *client.GitHubClient, repo repository.Repository, number int, milestone *github.Milestone) (*github.Milestone, error) {
+	return g.EditMilestone(ctx, repo.Owner, repo.Name, number, milestone)
+}
+
+// DeleteMilestone deletes a milestone by number for a repository (wrapper)
+func DeleteMilestone(ctx context.Context, g *client.GitHubClient, repo repository.Repository, number int) error {
+	return g.DeleteMilestone(ctx, repo.Owner, repo.Name, number)
+}
+
+func ListLabelsForMilestone(ctx context.Context, g *client.GitHubClient, repo repository.Repository, number int) ([]*github.Label, error) {
+	return g.ListLabelsForMilestone(ctx, repo.Owner, repo.Name, number)
+}
