78fed3e6e673a9ee472098e3b678e43b865bae88
srz_zumix <zumix.cpp@gmail.com>
2025-08-17 13:41:48 +0900

    feat: parse filepath (#14)
    * feat: parse filepath
    
    * fix lint

--- pkg/parser/repository.go
+++ pkg/parser/repository.go
@@ -2,6 +2,9 @@ package parser
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/cli/go-gh/v2/pkg/auth"
 	"github.com/cli/go-gh/v2/pkg/repository"
@@ -16,7 +19,10 @@ func RepositoryInput(input string) RepositoryOption {
 		}
 		repo, err := repository.Parse(input)
 		if err != nil {
-			return err
+			repo, err = parseFilePath(input)
+			if err != nil {
+				return fmt.Errorf(`expected the "[HOST/]OWNER/REPO" format or PATH, got %q`, input)
+			}
 		}
 		if r.Host != "" && r.Host != repo.Host {
 			return errors.New("conflicting host")
@@ -78,4 +84,46 @@ func Repository(opts ...RepositoryOption) (repository.Repository, error) {
 		return repository.Current()
 	}
 	return r, nil
+}
+
+func GetRepositoryFullName(repo repository.Repository) string {
+	return fmt.Sprintf("%s/%s", repo.Owner, repo.Name)
+}
+
+func GetRepositoryFullNameWithHost(repo repository.Repository) string {
+	if repo.Host != "" {
+		return fmt.Sprintf("%s/%s/%s", repo.Host, repo.Owner, repo.Name)
+	}
+	return fmt.Sprintf("%s/%s", repo.Owner, repo.Name)
+}
+
+func parseFilePath(input string) (repo repository.Repository, err error) {
+	if input == "" {
+		return repo, nil
+	}
+	// Check if the file path exists
+	input += "/.git"
+	if _, err := os.Stat(input); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return repo, errors.New("file path does not exist")
+		}
+		return repo, err
+	}
+	absPath, err := filepath.Abs(input)
+	if err != nil {
+		return repo, fmt.Errorf("failed to get absolute path: %w", err)
+	}
+	gitDir := os.Getenv("GIT_DIR")
+	defer func() {
+		if gitDir == "" {
+			err = os.Unsetenv("GIT_DIR")
+		} else {
+			err = os.Setenv("GIT_DIR", gitDir)
+		}
+	}()
+	err = os.Setenv("GIT_DIR", absPath)
+	if err != nil {
+		return repo, err
+	}
+	return repository.Current()
 }
