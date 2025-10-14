dcfd534dc3c3ca767fc9d15890826120004f5752
srz_zumix <zumix.cpp@gmail.com>
2025-10-14 21:26:17 +0900

    add reviewers api

--- pkg/gh/pr.go
+++ pkg/gh/pr.go
@@ -3,10 +3,28 @@ package gh
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/cli/go-gh/v2/pkg/repository"
 	"github.com/google/go-github/v73/github"
 )
+
+type ReviewersRequest struct {
+	Reviewers     []string
+	TeamReviewers []string
+}
+
+func GetRequestedReviewers(reviewers []string) ReviewersRequest {
+	reviewersRequest := ReviewersRequest{}
+	for _, reviewer := range reviewers {
+		if strings.Contains(reviewer, "/") {
+			reviewersRequest.TeamReviewers = append(reviewersRequest.TeamReviewers, reviewer)
+		} else {
+			reviewersRequest.Reviewers = append(reviewersRequest.Reviewers, reviewer)
+		}
+	}
+	return reviewersRequest
+}
 
 func GetPullRequestNumberFromString(pr string) (int, error) {
 	return GetIssueNumberFromString(pr)
@@ -50,6 +68,42 @@ func ListPullRequestFiles(ctx context.Context, g *GitHubClient, repo repository.
 		return nil, fmt.Errorf("failed to list files for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
 	}
 	return files, nil
+}
+
+func RequestPullRequestReviewers(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, reviewersRequest ReviewersRequest) (*github.PullRequest, error) {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	pr, err := g.RequestReviewers(ctx, repo.Owner, repo.Name, number, reviewersRequest.Reviewers, reviewersRequest.TeamReviewers)
+	if err != nil {
+		return nil, fmt.Errorf("failed to request reviewers for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+	}
+	return pr, nil
+}
+
+func RemovePullRequestReviewers(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, reviewersRequest ReviewersRequest) error {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	err = g.RemoveReviewers(ctx, repo.Owner, repo.Name, number, reviewersRequest.Reviewers, reviewersRequest.TeamReviewers)
+	if err != nil {
+		return fmt.Errorf("failed to remove reviewers for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+	}
+	return nil
+}
+
+func ListPullRequestReviewers(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any) (*github.Reviewers, error) {
+	number, err := GetPullRequestNumber(pull_request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pull request number from '%s': %w", pull_request, err)
+	}
+	reviewers, err := g.ListRequestedReviewers(ctx, repo.Owner, repo.Name, number)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list reviewers for pull request #%d in repository '%s/%s': %w", number, repo.Owner, repo.Name, err)
+	}
+	return reviewers, nil
 }
 
 func SetPullRequestLabels(ctx context.Context, g *GitHubClient, repo repository.Repository, pull_request any, labels []string) ([]*github.Label, error) {
