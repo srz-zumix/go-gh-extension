b5c84cf75766152227e9555346e3af73d1ffb503
srz_zumix <zumix.cpp@gmail.com>
2025-11-03 21:42:15 +0900

    add MutuallyExclusiveBoolFlags (#44)
    * add MutuallyExclusiveBoolFlags
    
    * up

---
+++ pkg/cmdflags/mutally-exclusive-bool.go
@@ -0,0 +1,45 @@
+package cmdflags
+
+import "github.com/spf13/cobra"
+
+type MutuallyExclusiveBoolFlags struct {
+	Enabled  bool
+	Disabled bool
+}
+
+func (m *MutuallyExclusiveBoolFlags) AddFlag(cmd *cobra.Command, name string, enableCaseUsage string, disableCaseUsage string) {
+	cmd.Flags().BoolVar(&m.Enabled, name, false, enableCaseUsage)
+	cmd.Flags().BoolVar(&m.Disabled, "no-"+name, false, disableCaseUsage)
+	cmd.MarkFlagsMutuallyExclusive(name, "no-"+name)
+}
+
+func (m *MutuallyExclusiveBoolFlags) IsEnabled() bool {
+	return m.Enabled
+}
+
+func (m *MutuallyExclusiveBoolFlags) IsTrue() bool {
+	return m.Enabled
+}
+
+func (m *MutuallyExclusiveBoolFlags) IsDisabled() bool {
+	return m.Disabled
+}
+
+func (m *MutuallyExclusiveBoolFlags) IsFalse() bool {
+	return m.Disabled
+}
+
+func (m *MutuallyExclusiveBoolFlags) IsSet() bool {
+	return m.Enabled || m.Disabled
+}
+
+func (m *MutuallyExclusiveBoolFlags) GetValue() *bool {
+	if m.Enabled {
+		val := true
+		return &val
+	} else if m.Disabled {
+		val := false
+		return &val
+	}
+	return nil
+}
