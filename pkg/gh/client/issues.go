123270ab94b238ea65d0ff242f4c0c7c22cfe524
srz_zumix <zumix.cpp@gmail.com>
2026-04-10 11:48:06 +0900

    comment hide (#192)

--- pkg/gh/client/issues.go
+++ pkg/gh/client/issues.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/google/go-github/v84/github"
+	"github.com/shurcooL/githubv4"
 )
 
 // GetIssueByNumber retrieves an issue by owner, repo, and issue number
@@ -101,4 +102,34 @@ func (g *GitHubClient) ListIssueComments(ctx context.Context, owner string, repo
 		opt.Page = resp.NextPage
 	}
 	return allComments, nil
+}
+
+// MinimizeComment hides (minimizes) a comment using the GraphQL minimizeComment mutation.
+// classifier must be one of: ABUSE, DUPLICATE, OFF_TOPIC, OUTDATED, RESOLVED, SPAM.
+func (g *GitHubClient) MinimizeComment(ctx context.Context, nodeID string, classifier string) error {
+	graphql, err := g.GetOrCreateGraphQLClient()
+	if err != nil {
+		return err
+	}
+
+	var mutation struct {
+		MinimizeComment struct {
+			MinimizedComment struct {
+				IsMinimized     githubv4.Boolean
+				MinimizedReason githubv4.String
+			}
+		} `graphql:"minimizeComment(input: $input)"`
+	}
+
+	type MinimizeCommentInput struct {
+		SubjectID  githubv4.ID     `json:"subjectId"`
+		Classifier githubv4.String `json:"classifier"`
+	}
+
+	input := MinimizeCommentInput{
+		SubjectID:  githubv4.ID(nodeID),
+		Classifier: githubv4.String(classifier),
+	}
+
+	return graphql.Mutate(ctx, &mutation, input, nil)
 }
