b7d2b6ccc65ac77ccc743a27bff2b654fe63cd34
srz_zumix <zumix.cpp@gmail.com>
2026-07-11 08:01:59 +0900

    fix

--- pkg/gh/client/code_scanning_codeql.go
+++ pkg/gh/client/code_scanning_codeql.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/google/go-github/v88/github"
 )
@@ -27,7 +28,7 @@ func (g *GitHubClient) GetCodeQLDatabase(ctx context.Context, owner, repo, langu
 
 // DeleteCodeQLDatabase deletes a CodeQL database for a language in a repository.
 func (g *GitHubClient) DeleteCodeQLDatabase(ctx context.Context, owner, repo, language string) error {
-	u := fmt.Sprintf("repos/%v/%v/code-scanning/codeql/databases/%v", owner, repo, language)
+	u := fmt.Sprintf("repos/%v/%v/code-scanning/codeql/databases/%v", owner, repo, url.PathEscape(language))
 	req, err := g.client.NewRequest(ctx, "DELETE", u, nil)
 	if err != nil {
 		return err
